12d7d722c1cb7a50bb7605641454ff592cec540b
Aditya Raj <adityaraj56558@gmail.com>
2026-06-07 19:54:19 +0530

    Day 6-Fibonacci Sequence

---
+++ Fibonacci.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int fib(int n){
+    if(n<=1){
+        return 1;
+    }
+    return fib(n-1)+fib(n-2);
+}
+int main(){
+    int n;
+    cout<<"Enter the number of n: ";
+    cin>>n;
+    cout<<"The nth Fibonacci number is: "<<fib(n)<<endl;
+    return 0;
+}
